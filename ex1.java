fec3ac54989d790e4c1eaa86802c104538292995
Andy <perecpon@yandex.ru>
2023-02-16 16:52:43 +0300

    1.3

--- ex1.java
+++ ex1.java
@@ -1,3 +1,7 @@
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n),
+// n! (произведение чисел от 1 до n)
+
+
 public class ex1 {
     public static void main(String[] args) {
         System.out.println(triangleSum(7));
